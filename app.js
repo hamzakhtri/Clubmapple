5ab45803cd732a5d9f0b493e39534d4a2dab7942
unknown <hamzakhatri12345@gmail.com>
2023-11-26 20:10:57 +0500

    clubmapple

---
+++ app.js
@@ -0,0 +1,39 @@
+(function () {
+    "use strict";
+
+    var carousels = function () {
+        $(".owl-carousel1").owlCarousel({
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: true,
+            dots:true,
+            pagination:true,
+            mouseDrag:false,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            grap: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                680: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                },
+                1000: {
+                    items: 3,
+                    nav: true
+                }
+            }
+        });
+    };
+
+    (function ($) {
+        carousels();
+    })(jQuery);
+})();
